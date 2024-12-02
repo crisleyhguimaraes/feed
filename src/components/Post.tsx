104e0db421b65bb6e2d9b4df36533554b82eed5c
Crisley Guimarães <43907168+crisleyhguimaraes@users.noreply.github.com>
2024-12-02 17:50:33 -0300

    fix: resolve compatibility issues with TypeScript in the project

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,13 +1,30 @@
 import { format, formatDistanceToNow } from "date-fns";
-import ptBR from "date-fns/locale/pt-BR";
-import { useState } from "react";
+import { ptBR } from "date-fns/locale/pt-BR";
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
 
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 
 import styles from "./Post.module.css";
 
-export function Post({ author, publishedAt, content }) {
+interface Author {
+    name: string;
+    avatarUrl: string;
+    role: string;
+}
+
+interface Content {
+    type: "paragraph" | "link";
+    content: string;
+}
+
+interface PostProps {
+    author: Author;
+    publishedAt: Date;
+    content: Content[];
+}
+
+export function Post({ author, publishedAt, content }: PostProps) {
     const [comments, setComments] = useState(["Post muito bacana, hein?!"]);
 
     const [newCommentText, setNewCommentText] = useState("");
@@ -25,28 +42,24 @@ export function Post({ author, publishedAt, content }) {
         addSuffix: true,
     });
 
-    function handleCrateNewComment() {
+    function handleCrateNewComment(event: FormEvent) {
         event.preventDefault();
 
         setComments([...comments, newCommentText]);
         setNewCommentText("");
     }
 
-    function handleNewCommentChange() {
+    function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
         event.target.setCustomValidity("");
 
         setNewCommentText(event.target.value);
     }
 
-    function handleNewCommentInvalid() {
+    function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
         event.target.setCustomValidity("Esse campo é obrigatório!");
     }
 
-    function handleNewCommentInvalid() {
-        return newCommentText.trim() === "";
-    }
-
-    function deleteComment(commentToDelete) {
+    function deleteComment(commentToDelete: string) {
         const commentsWithoutDeletedOne = comments.filter((comment) => {
             return comment !== commentToDelete;
         });
